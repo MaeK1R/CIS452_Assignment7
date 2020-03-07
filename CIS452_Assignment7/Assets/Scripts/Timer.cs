76cc0dc55101039e38b2be702a2040b29d904e42
MaeK1R <56087293+MaeK1R@users.noreply.github.com>
2020-03-06 23:35:27 -0600

    timer fail state

---
+++ CIS452_Assignment7/Assets/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 90f;
+
+    public Text countDownText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countDownText.text = currentTime.ToString("Time: 0");
+
+        if(currentTime <= 0)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
